0850632512437aee56cb0e93a5d9275889b30148
Stephen Jingle <gitlog@sib.li>
2025-05-01 01:12:20 +0000

    fix bare example

--- stdout_writeability/cout.cpp
+++ stdout_writeability/cout.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 
+// g++ -std=c++11 cout.cpp -o cout
+// ./cout > /dev/full
 int main() {
     std::ostream out(std::cout.rdbuf());
-
-    // Does not work either:
-    // g++ -std=c++11 cout.cpp -o cout
-    // ./cout > /dev/full
+    
+    out << "Hello cout" << std::endl;
     if (out.fail()) {
         throw std::runtime_error("could not write to stdout");
     }
-    std::cout << "Hello cout" << std::endl;
-
+    
     return 0;
 }
